ccb753107a375a2202b16d9fcdd99ccfbd155f56
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-02-28 12:45:31 +0100

    setting up the main pages

---
+++ pages/events/[...slug]/index.js
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>Show Filtered Events</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function StartingPage() {
+  return (
+    <div>
+      <h1>Home Page - Show Featured Events</h1>
+    </div>
+  );
+}
+
+export default StartingPage;
